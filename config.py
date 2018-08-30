64dc744c092951732407f1d72678c9ab71881a4e
Oleksandr Yeropudov <oleksandr.yeropudov@gmail.com>
2018-08-30 10:18:54 +0300

    issue #4 close #4  add requirements add docker-compose init readme

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://epicrpg:dtfbk11gh@127.0.0.1:3307/epic'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://epicrpg:e36BbwM2x6Tm@127.0.0.1:3307/epic'
     JWT_KEY = 's9A1NDxIkPAqEIQHThy5Oh'
