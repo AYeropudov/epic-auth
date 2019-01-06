54a90bbc8157bfc2b9dcf2679222551199f94bf5
alex <oleksandr.yeropudov@gmail.com>
2019-01-06 20:21:15 +0300

    docker

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://epicrpg:e36BbwM2x6Tm@127.0.0.1/epic'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://epicrpg:e36BbwM2x6Tm@sqldb/epic'
     JWT_KEY = 's9A1NDxIkPAqEIQHThy5Oh'
     BASE_DIR = basedir
