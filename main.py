fa47d3f7191ee4682ee8c523cf033bb5f189d34b
Alex Yeropudov <oleksandr.yeropudov@gmail.com>
2019-05-30 14:35:29 +0300

    fixes Docker

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from app import app
 from app import db
 from app.models import User, Token
-
+import pydevd_pycharm
 
 @app.shell_context_processor
 def make_shell_context():
@@ -9,4 +9,6 @@ def make_shell_context():
 
 
 if __name__ == '__main__':
+    if app.config.get('FLASK_DEBUG'):
+        pydevd_pycharm.settrace('192.168.255.1', port=37259, stdoutToServer=True, stderrToServer=True)
     app.run(host="0.0.0.0", debug=True)
